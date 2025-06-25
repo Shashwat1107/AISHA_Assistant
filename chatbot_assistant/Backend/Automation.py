841abb6dbaca7767b5618e959d453b8a60e8ff7e
Shashwat1107 <83599795+Shashwat1107@users.noreply.github.com>
2025-06-25 19:49:28 +0530

    Delete chatbot_assistant/Backend directory
    The repository has some fatal errors, the changes are not getting committed

--- chatbot_assistant/Backend/Automation.py
+++
@@ -1,6 +0,0 @@
-from AppOpener import close,open as appopen
-from webbrowser import open as webopen
-from pywhatkit import search, playonyt
-from dotenv import dotenv_values
-from bs4 import BeautifulSoup
-from rich import print 
